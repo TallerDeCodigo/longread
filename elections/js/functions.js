a4b9a426807c149691680543a74f6228403566c8
helagone <luah613@gmail.com>
2016-11-07 16:29:09 -0600

    ajax

--- elections/js/functions.js
+++ elections/js/functions.js
@@ -72,21 +72,21 @@
 
         $.ajax({
          	//url: 'http://static-feeds.esmas.com/awsfeeds/noticieros/mix/eleccionesEstadosUnidos2016.js',
-        	url: './js/json_news.json',
-         	type: 'get',
-         	//dataType: 'jsonp'
-         	dataType: 'json'
+        		url: './js/json_news.json',
+         		type: 'get',
+         		dataType: 'jsonp',
+         		jsonpCallback: 'elecccionesUSA',
+        		jsonp: false,
+         		//dataType: 'json'
 		      })
 		   		.done(function(data) {
+		   			var fecha_actual = new Date();
+
+		   			console.log(data);
 		   			var j = JSON.stringify(data);
-		   			//console.log(j);
 		   			data = j.replace('elecccionesUSA(', '');
 		   			data = j.replace(')', '');
-
 		   			data = JSON.parse(data);
-		   			console.log(data);
-
-		   			var fecha_actual = new Date();
 
 		   			$.each(data, function(index, value) {
 		   				var title = '';
@@ -97,26 +97,17 @@
 
 
 		   				$.each(value, function(index, value){
-		   					//console.log(index);
-		   					
-		   					//console.log(index + ' ' + value);
 
 		   					if(index == 'title'){
-		   						// add title to html
 		   						title = value;
 		   					}
 
 		   					if(index == 'link'){
-		   						// add link to html	
 		   						link = value;
-
-		   						//console.log(link);
 		   					}
 		   					
 		   					if(index == 'images'){
-		   						// add img to html
 		   						$.each(value, function(index, value){
-		   							//console.log(index + ' --- ' + value);
 		   							if(index === 'img_624x468'){
 										image = value;
 									}	
@@ -126,44 +117,32 @@
 		   					if(index == 'pubdate'){
 		   						fecha = value;
 		   						d = new Date(fecha);
-
 		   						d = fecha_actual - d;
-
 		   						d = ((d/1000)/60)/60;
-
 		   						d = Math.round(d);
-
-
 
 		   						if(d > 24){
 		   							d =  d / 24;
 		   							d = Math.round(d);
-
 		   							d+= ' Días';
 		   						}else{
-
 		   							d+= ' Horas'
 		   						}
-
-
-
 		   					}
 		   					
 		   				});
 		   				$('.notas').append('<li class="clearfix"><a href="' + link + '"><div class="img_frame"><img src="' + image +'"></div></a><h5><a href="' + link + '">' + title +'</a></h5><span class="_time">hace ' + d + '</span></li>');
 		   			});
 
-		      	console.log("success");
+		   			
 		   })
 		   		.fail(function() {
-		      	console.log("error");
+		      	console.log("fail");
 		   })
 		   		.always(function() {
 		      	console.log("complete");
    });
-
-
-	});
+});
 
 	
 
