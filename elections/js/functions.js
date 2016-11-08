e47e70f7ed631ce6a47b190fffb0852795183a2f
manuel <molop3@gmail.com>
2016-11-08 13:25:59 -0600

    colorcitos

--- elections/js/functions.js
+++ elections/js/functions.js
@@ -54,7 +54,7 @@
     		$('.info_box.map1').css({left:  e.pageX - 120,top:   e.pageY - tip});
     		$('.info_box.map1 h3').html($(this).attr('full-name'));
     		$('.info_box.map1 th.ve').html($(this).attr('votos'));
-    		$('.info_box.map1 th.peso').html($(this).attr('peso'));
+    		$('.info_box.map1 th.peso').html($(this).attr('peso')+'%');
     		$('.info_box.map1 th.hora').html($(this).attr('hora'));
     		$('.info_box.map1').show();
     		setTimeout(function() {
@@ -93,6 +93,15 @@
             $(this).hide();
         });
 
+        $("div.barra div").click(function(){
+    		$('.big_bar').hide();
+        	if ($(this).parent().parent().hasClass('hc')) {
+        		$('.big_bar.hc').show();
+        	} else {
+        		$('.big_bar.dt').show();
+        	}
+        });
+
         $("div.iframe").mouseout(function() {
 			setTimeout(function() {
 				$(this).find('div.over_iframe').show();
@@ -121,26 +130,56 @@
 			   			var tend = '';
 			   			var siva1 = false;
 			   			var siva2 = false;
+			   			var peso_hc = 0;
+						var peso_dt = 0;
 			   			console.log(value);
    			   			$.each(value, function(key, value) {
    				   			if (key == 'Estado') { etdo = value }
    				   			if (key == 'Abr') { abre = value }
    				   			if (key == 'Ve') { voto = value }
-   				   			if (key == 'Porcentaje') { peso = parseFloat(value); peso = peso * 100; peso = peso.toFixed(2) + '%'; }
+   				   			if (key == 'Porcentaje') { peso = parseFloat(value); peso = peso * 100; peso = peso.toFixed(2); }
    				   			if (key == 'Hora') { hora = value }
    				   			if (key == 'Tendencia') { tend = value }
    				   			if (key == 'Democratas') { demo = value }
    				   			if (key == 'Republicanos') { repu = value }
-   				   			if (key == 'Votos_d') { suma_hc += parseInt(value); if (value!=0) { siva1=true } }
-   				   			if (key == 'Votos_r') { suma_dt += parseInt(value); if (value!=0) { siva2=true } }
+   				   			if (key == 'Votos_d') { suma_hc += parseInt(value); peso_hc=value; if (value!=0) { siva1=true } }
+   				   			if (key == 'Votos_r') { suma_dt += parseInt(value); peso_dt=value; if (value!=0) { siva2=true } }
    			   			});
 
-   			   			if (siva1) {
-   			   				$('.big_bar.hc').append('<div data-name="'+abre+'" style="width:'+peso+';"></div>');
+   			   			if (abre=='NE' || abre=='ME') {
+   			   				if (siva1) {
+   			   					var nuevo = (peso*peso_hc)/voto;
+	   			   				$('.big_bar.hc').append('<div data-name="'+abre+'" style="width:'+nuevo+'%;"></div>');
+	   			   			}
+	   			   			if (siva2) {
+	   			   				var nuevo = (peso*peso_dt)/voto;
+	   			   				$('.big_bar.dt').append('<div data-name="'+abre+'" style="width:'+nuevo+'%;"></div>');
+	   			   			}
+
+   			   				if (peso_hc>peso_dt) {
+   			   					$('#map1 .state[data-name="'+abre+'"]').css('fill','#003594');
+   			   				} else {
+   			   					$('#map1 .state[data-name="'+abre+'"]').css('fill','#EB0029');
+   			   				}
+   			   				$('#map2 g.state[data-name="'+abre+'"] polygon').css('fill','#EB0029');
+		   					for (var i = 1; i <= peso_hc; i++) {
+		   						$('#map2 g.state[data-name="'+abre+'"] polygon:nth-of-type('+i+')').css('fill','#003594');
+		   					}
+   			   			} else {
+	   			   			if (siva1) {
+	   			   				$('.big_bar.hc').append('<div data-name="'+abre+'" style="width:'+peso+'%;"></div>');
+	   			   				$('#map1 .state[data-name="'+abre+'"]').css('fill','#003594');
+	   			   				$('#map2 g.state[data-name="'+abre+'"] polygon').css('fill','#003594');
+	   			   				$('#map2 g.state[data-name="'+abre+'"] polyline').css('fill','#003594');
+	   			   			}
+	   			   			if (siva2) {
+	   			   				$('.big_bar.dt').append('<div data-name="'+abre+'" style="width:'+peso+'%;"></div>');
+	   			   				$('#map1 .state[data-name="'+abre+'"]').css('fill','#EB0029');
+	   			   				$('#map2 g.state[data-name="'+abre+'"] polygon').css('fill','#EB0029');
+	   			   				$('#map2 g.state[data-name="'+abre+'"] polyline').css('fill','#EB0029');
+	   			   			}
    			   			}
-   			   			if (siva2) {
-   			   				$('.big_bar.dt').append('<div data-name="'+abre+'" style="width:'+peso+';"></div>');
-   			   			}
+
 
    			   			$(".big_bar.hc div").each(function(){
 		   			        if ($(this).attr('data-name') === '') {
@@ -167,13 +206,13 @@
 		   			});
 
 		   			var calculo1 = (suma_hc/270)*100;
-		   			$('.middle.hc barra div').width(calculo1+'%');
-		   			$('.middle.hc h5').html(calculo1+'%');
+		   			$('.middle.hc .barra div').width(calculo1+'%');
+		   			$('.middle.hc h5').html(calculo1.toFixed(1)+'%');
 		   			$('.middle.hc span strong').html(suma_hc);
 
 		   			var calculo2 = (suma_dt/270)*100;
-		   			$('.middle.dt barra div').width(calculo2+'%');
-		   			$('.middle.dt h5').html(calculo2+'%');
+		   			$('.middle.dt .barra div').width(calculo2+'%');
+		   			$('.middle.dt h5').html(calculo2.toFixed(1)+'%');
 		   			$('.middle.dt span strong').html(suma_dt);
 
 		   			var calculo3 = total - suma_hc - suma_dt;
@@ -198,7 +237,7 @@
 		   		.done(function(data) {
 		   			var fecha_actual = new Date();
 
-		   			console.log(data);
+		   			// console.log(data);
 		   			var j = JSON.stringify(data);
 		   			data = j.replace('elecccionesUSA(', '');
 		   			data = j.replace(')', '');
