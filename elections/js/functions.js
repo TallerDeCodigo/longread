383c00c2556b28099fd8c8ff5b8c000e9ca7d1c7
hever <hever@tallerdecodigo.com>
2016-11-07 22:24:50 -0600

    shares

--- elections/js/functions.js
+++ elections/js/functions.js
@@ -174,8 +174,8 @@
    		});
 
         $.ajax({
-         	//url: 'http://static-feeds.esmas.com/awsfeeds/noticieros/mix/eleccionesEstadosUnidos2016.js',
-        		url: './js/json_news.json',
+         		url: 'http://static-feeds.esmas.com/awsfeeds/noticieros/mix/eleccionesEstadosUnidos2016.js',
+        		//url: './js/json_news.json',
          		type: 'get',
          		dataType: 'jsonp',
          		jsonpCallback: 'elecccionesUSA',
