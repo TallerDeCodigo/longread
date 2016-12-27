161c84a04b5da7dd5c7941b2bb2c1c50c536b53c
manuel <molop3@gmail.com>
2016-12-27 11:48:31 -0600

    fin de año

--- cuales-son-las-carreteras-mas-peligrosas-en-diciembre-navidad-ano-nuevo-en-mexico/js/functions.js
+++ cuales-son-las-carreteras-mas-peligrosas-en-diciembre-navidad-ano-nuevo-en-mexico/js/functions.js
@@ -133,225 +133,225 @@
 
 		
 
-		function relleno() {
+		// function relleno() {
 
-				var total = 538;
-				var suma_hc = 0;
-				var suma_dt = 0;
+		// 		var total = 538;
+		// 		var suma_hc = 0;
+		// 		var suma_dt = 0;
 				
-				$.ajax({
-		        		url: 'http://lisa.mx/clientes/elecciones/presidencia.php',
-		         		type: 'get',
-		         		dataType: 'json'
-				      })
-				   		.done(function(data) {
-				   			$.each(data, function(key, value) {
-					   			var etdo = '';
-					   			var abre = '';
-					   			var voto = '';
-					   			var peso = '';
-					   			var hora = '';
-					   			var demo = '';
-					   			var repu = '';
-					   			var tend = '';
-					   			var siva1 = false;
-					   			var siva2 = false;
-					   			var peso_hc = 0;
-								var peso_dt = 0;
-					   			console.log(value);
-		   			   			$.each(value, function(key, value) {
-		   				   			if (key == 'Estado') { etdo = value }
-		   				   			if (key == 'Abr') { abre = value }
-		   				   			if (key == 'Ve') { voto = value }
-		   				   			if (key == 'Porcentaje') { peso = parseFloat(value); peso = peso * 100; peso = peso.toFixed(2); }
-		   				   			if (key == 'Hora') { hora = value }
-		   				   			if (key == 'Tendencia') { tend = value }
-		   				   			if (key == 'Democratas') { demo = value }
-		   				   			if (key == 'Republicanos') { repu = value }
-		   				   			if (key == 'Votos_d') { suma_hc += parseInt(value); peso_hc=value; if (value!=0) { siva1=true } }
-		   				   			if (key == 'Votos_r') { suma_dt += parseInt(value); peso_dt=value; if (value!=0) { siva2=true } }
-		   			   			});
+		// 		$.ajax({
+		//         		url: 'http://lisa.mx/clientes/elecciones/presidencia.php',
+		//          		type: 'get',
+		//          		dataType: 'json'
+		// 		      })
+		// 		   		.done(function(data) {
+		// 		   			$.each(data, function(key, value) {
+		// 			   			var etdo = '';
+		// 			   			var abre = '';
+		// 			   			var voto = '';
+		// 			   			var peso = '';
+		// 			   			var hora = '';
+		// 			   			var demo = '';
+		// 			   			var repu = '';
+		// 			   			var tend = '';
+		// 			   			var siva1 = false;
+		// 			   			var siva2 = false;
+		// 			   			var peso_hc = 0;
+		// 						var peso_dt = 0;
+		// 			   			console.log(value);
+		//    			   			$.each(value, function(key, value) {
+		//    				   			if (key == 'Estado') { etdo = value }
+		//    				   			if (key == 'Abr') { abre = value }
+		//    				   			if (key == 'Ve') { voto = value }
+		//    				   			if (key == 'Porcentaje') { peso = parseFloat(value); peso = peso * 100; peso = peso.toFixed(2); }
+		//    				   			if (key == 'Hora') { hora = value }
+		//    				   			if (key == 'Tendencia') { tend = value }
+		//    				   			if (key == 'Democratas') { demo = value }
+		//    				   			if (key == 'Republicanos') { repu = value }
+		//    				   			if (key == 'Votos_d') { suma_hc += parseInt(value); peso_hc=value; if (value!=0) { siva1=true } }
+		//    				   			if (key == 'Votos_r') { suma_dt += parseInt(value); peso_dt=value; if (value!=0) { siva2=true } }
+		//    			   			});
 
-		   			   			if (abre=='NE' || abre=='ME') {
-		   			   				if (siva1) {
-		   			   					var nuevo = (peso*peso_hc)/voto;
-			   			   				$('.big_bar.hc').append('<div data-name="'+abre+'" style="width:'+nuevo+'%;"></div>');
-			   			   			}
-			   			   			if (siva2) {
-			   			   				var nuevo = (peso*peso_dt)/voto;
-			   			   				$('.big_bar.dt').append('<div data-name="'+abre+'" style="width:'+nuevo+'%;"></div>');
-			   			   			}
+		//    			   			if (abre=='NE' || abre=='ME') {
+		//    			   				if (siva1) {
+		//    			   					var nuevo = (peso*peso_hc)/voto;
+		// 	   			   				$('.big_bar.hc').append('<div data-name="'+abre+'" style="width:'+nuevo+'%;"></div>');
+		// 	   			   			}
+		// 	   			   			if (siva2) {
+		// 	   			   				var nuevo = (peso*peso_dt)/voto;
+		// 	   			   				$('.big_bar.dt').append('<div data-name="'+abre+'" style="width:'+nuevo+'%;"></div>');
+		// 	   			   			}
 
-		   			   				if (siva1 || siva2) {
-		   			   				if (peso_hc>peso_dt) {
-		   			   					$('#map1 .state[data-name="'+abre+'"]').css('fill','#003594');
-		   			   					$('.state[data-name="'+abre+'"]').addClass('bluee');
-		   			   				} else {
-		   			   					$('#map1 .state[data-name="'+abre+'"]').css('fill','#EB0029');
-		   			   					$('.state[data-name="'+abre+'"]').addClass('redd');
-		   			   				}
-			   			   				$('#map2 g.state[data-name="'+abre+'"] polygon').css('fill','#EB0029');
-					   					for (var i = 1; i <= peso_hc; i++) {
-					   						$('#map2 g.state[data-name="'+abre+'"] polygon:nth-of-type('+i+')').css('fill','#003594');
-					   					}
-		   			   				}
-		   			   			} else {
-			   			   			if (siva1) {
-			   			   				$('.big_bar.hc').append('<div data-name="'+abre+'" style="width:'+peso+'%;"></div>');
-			   			   				$('#map1 .state[data-name="'+abre+'"]').css('fill','#003594');
-			   			   				$('.state[data-name="'+abre+'"]').addClass('bluee');
-			   			   				$('#map2 g.state[data-name="'+abre+'"] polygon').css('fill','#003594');
-			   			   				$('#map2 g.state[data-name="'+abre+'"] polyline').css('fill','#003594');
-			   			   			}
-			   			   			if (siva2) {
-			   			   				$('.big_bar.dt').append('<div data-name="'+abre+'" style="width:'+peso+'%;"></div>');
-			   			   				$('#map1 .state[data-name="'+abre+'"]').css('fill','#EB0029');
-			   			   				$('.state[data-name="'+abre+'"]').addClass('redd');
-			   			   				$('#map2 g.state[data-name="'+abre+'"] polygon').css('fill','#EB0029');
-			   			   				$('#map2 g.state[data-name="'+abre+'"] polyline').css('fill','#EB0029');
-			   			   			}
-		   			   			}
+		//    			   				if (siva1 || siva2) {
+		//    			   				if (peso_hc>peso_dt) {
+		//    			   					$('#map1 .state[data-name="'+abre+'"]').css('fill','#003594');
+		//    			   					$('.state[data-name="'+abre+'"]').addClass('bluee');
+		//    			   				} else {
+		//    			   					$('#map1 .state[data-name="'+abre+'"]').css('fill','#EB0029');
+		//    			   					$('.state[data-name="'+abre+'"]').addClass('redd');
+		//    			   				}
+		// 	   			   				$('#map2 g.state[data-name="'+abre+'"] polygon').css('fill','#EB0029');
+		// 			   					for (var i = 1; i <= peso_hc; i++) {
+		// 			   						$('#map2 g.state[data-name="'+abre+'"] polygon:nth-of-type('+i+')').css('fill','#003594');
+		// 			   					}
+		//    			   				}
+		//    			   			} else {
+		// 	   			   			if (siva1) {
+		// 	   			   				$('.big_bar.hc').append('<div data-name="'+abre+'" style="width:'+peso+'%;"></div>');
+		// 	   			   				$('#map1 .state[data-name="'+abre+'"]').css('fill','#003594');
+		// 	   			   				$('.state[data-name="'+abre+'"]').addClass('bluee');
+		// 	   			   				$('#map2 g.state[data-name="'+abre+'"] polygon').css('fill','#003594');
+		// 	   			   				$('#map2 g.state[data-name="'+abre+'"] polyline').css('fill','#003594');
+		// 	   			   			}
+		// 	   			   			if (siva2) {
+		// 	   			   				$('.big_bar.dt').append('<div data-name="'+abre+'" style="width:'+peso+'%;"></div>');
+		// 	   			   				$('#map1 .state[data-name="'+abre+'"]').css('fill','#EB0029');
+		// 	   			   				$('.state[data-name="'+abre+'"]').addClass('redd');
+		// 	   			   				$('#map2 g.state[data-name="'+abre+'"] polygon').css('fill','#EB0029');
+		// 	   			   				$('#map2 g.state[data-name="'+abre+'"] polyline').css('fill','#EB0029');
+		// 	   			   			}
+		//    			   			}
 
 
-		   			   			$(".big_bar.hc div").each(function(){
-				   			        if ($(this).attr('data-name') === '') {
-				   			            $(this).remove();
-				   			        }                               
-				   			    });
+		//    			   			$(".big_bar.hc div").each(function(){
+		// 		   			        if ($(this).attr('data-name') === '') {
+		// 		   			            $(this).remove();
+		// 		   			        }                               
+		// 		   			    });
 		   			   			
 
-		   			   			$('#map1 .state[data-name="'+abre+'"]').attr('full-name',etdo);
-		   			   			$('#map1 .state[data-name="'+abre+'"]').attr('votos',voto);
-		   			   			$('#map1 .state[data-name="'+abre+'"]').attr('peso',peso);
-		   			   			$('#map1 .state[data-name="'+abre+'"]').attr('hora',hora);
-		   			   			$('#map1 .state[data-name="'+abre+'"]').attr('democrata',demo);
-		   			   			$('#map1 .state[data-name="'+abre+'"]').attr('republicano',repu);
-		   			   			$('#map1 .state[data-name="'+abre+'"]').attr('tendencia',tend);
+		//    			   			$('#map1 .state[data-name="'+abre+'"]').attr('full-name',etdo);
+		//    			   			$('#map1 .state[data-name="'+abre+'"]').attr('votos',voto);
+		//    			   			$('#map1 .state[data-name="'+abre+'"]').attr('peso',peso);
+		//    			   			$('#map1 .state[data-name="'+abre+'"]').attr('hora',hora);
+		//    			   			$('#map1 .state[data-name="'+abre+'"]').attr('democrata',demo);
+		//    			   			$('#map1 .state[data-name="'+abre+'"]').attr('republicano',repu);
+		//    			   			$('#map1 .state[data-name="'+abre+'"]').attr('tendencia',tend);
 
-		   			   			$('#map2 .state[data-name="'+abre+'"]').attr('full-name',etdo);
-		   			   			$('#map2 .state[data-name="'+abre+'"]').attr('votos',voto);
-		   			   			$('#map2 .state[data-name="'+abre+'"]').attr('peso',peso);
-		   			   			$('#map2 .state[data-name="'+abre+'"]').attr('hora',hora);
-		   			   			$('#map2 .state[data-name="'+abre+'"]').attr('democrata',demo);
-		   			   			$('#map2 .state[data-name="'+abre+'"]').attr('republicano',repu);
-		   			   			$('#map2 .state[data-name="'+abre+'"]').attr('tendencia',tend);
-				   			});
+		//    			   			$('#map2 .state[data-name="'+abre+'"]').attr('full-name',etdo);
+		//    			   			$('#map2 .state[data-name="'+abre+'"]').attr('votos',voto);
+		//    			   			$('#map2 .state[data-name="'+abre+'"]').attr('peso',peso);
+		//    			   			$('#map2 .state[data-name="'+abre+'"]').attr('hora',hora);
+		//    			   			$('#map2 .state[data-name="'+abre+'"]').attr('democrata',demo);
+		//    			   			$('#map2 .state[data-name="'+abre+'"]').attr('republicano',repu);
+		//    			   			$('#map2 .state[data-name="'+abre+'"]').attr('tendencia',tend);
+		// 		   			});
 
-				   			var calculo1 = (suma_hc/270)*100;
-				   			$('.middle.hc .barra div').width(calculo1+'%');
-				   			$('.middle.hc h5').html(calculo1.toFixed(1)+'%');
-				   			$('.middle.hc span strong').html(suma_hc);
+		// 		   			var calculo1 = (suma_hc/270)*100;
+		// 		   			$('.middle.hc .barra div').width(calculo1+'%');
+		// 		   			$('.middle.hc h5').html(calculo1.toFixed(1)+'%');
+		// 		   			$('.middle.hc span strong').html(suma_hc);
 
-				   			var calculo2 = (suma_dt/270)*100;
-				   			$('.middle.dt .barra div').width(calculo2+'%');
-				   			$('.middle.dt h5').html(calculo2.toFixed(1)+'%');
-				   			$('.middle.dt span strong').html(suma_dt);
+		// 		   			var calculo2 = (suma_dt/270)*100;
+		// 		   			$('.middle.dt .barra div').width(calculo2+'%');
+		// 		   			$('.middle.dt h5').html(calculo2.toFixed(1)+'%');
+		// 		   			$('.middle.dt span strong').html(suma_dt);
 
-				   			var calculo3 = total - suma_hc - suma_dt;
-				   			$('.versus p strong').html(calculo3);
+		// 		   			var calculo3 = total - suma_hc - suma_dt;
+		// 		   			$('.versus p strong').html(calculo3);
 
-					   }).fail(function() {
-					      	console.log("fail");
+		// 			   }).fail(function() {
+		// 			      	console.log("fail");
 
-					   }).always(function() {
-					      	console.log("complete");
-		   		});
-		}
-
-
-		window.setInterval(relleno, 600000);
-
-		$(window).on("load", relleno );
-
-        $.ajax({
-         		url: 'http://static-feeds.esmas.com/awsfeeds/noticieros/mix/eleccionesEstadosUnidos2016.js',
-        		//url: './js/json_news.json',
-         		type: 'get',
-         		dataType: 'jsonp',
-         		jsonpCallback: 'elecccionesUSA',
-        		jsonp: false,
-         		//dataType: 'json'
-		      })
-		   		.done(function(data) {
-		   			var fecha_actual = new Date();
-
-		   			// console.log(data);
-		   			var j = JSON.stringify(data);
-		   			data = j.replace('elecccionesUSA(', '');
-		   			data = j.replace(')', '');
-		   			data = JSON.parse(data);
-		   			var contador = 1;
-
-		   			$.each(data, function(index, value) {
-		   				var title = '';
-   						var fecha = '';
-   						var link  = '';
-   						var image  = '';
-   						var d  = new Date;
+		// 			   }).always(function() {
+		// 			      	console.log("complete");
+		//    		});
+		// }
 
 
-		   				$.each(value, function(index, value){
+		// window.setInterval(relleno, 600000);
 
-		   					if(index == 'title'){
-		   						title = value;
-		   					}
+		// $(window).on("load", relleno );
 
-		   					if(index == 'link'){
-		   						link = value;
-		   					}
+   //      $.ajax({
+   //       		url: 'http://static-feeds.esmas.com/awsfeeds/noticieros/mix/eleccionesEstadosUnidos2016.js',
+   //      		//url: './js/json_news.json',
+   //       		type: 'get',
+   //       		dataType: 'jsonp',
+   //       		jsonpCallback: 'elecccionesUSA',
+   //      		jsonp: false,
+   //       		//dataType: 'json'
+		 //      })
+		 //   		.done(function(data) {
+		 //   			var fecha_actual = new Date();
+
+		 //   			// console.log(data);
+		 //   			var j = JSON.stringify(data);
+		 //   			data = j.replace('elecccionesUSA(', '');
+		 //   			data = j.replace(')', '');
+		 //   			data = JSON.parse(data);
+		 //   			var contador = 1;
+
+		 //   			$.each(data, function(index, value) {
+		 //   				var title = '';
+   // 						var fecha = '';
+   // 						var link  = '';
+   // 						var image  = '';
+   // 						var d  = new Date;
+
+
+		 //   				$.each(value, function(index, value){
+
+		 //   					if(index == 'title'){
+		 //   						title = value;
+		 //   					}
+
+		 //   					if(index == 'link'){
+		 //   						link = value;
+		 //   					}
 		   					
-		   					if(index == 'images'){
-		   						$.each(value, function(index, value){
-		   							if(index === 'img_624x468'){
-										image = value;
-									}	
-		   						});	
-		   					}
+		 //   					if(index == 'images'){
+		 //   						$.each(value, function(index, value){
+		 //   							if(index === 'img_624x468'){
+			// 							image = value;
+			// 						}	
+		 //   						});	
+		 //   					}
 
-		   					if(index == 'pubdate'){
-		   						fecha = value;
-		   						d = new Date(fecha);
-		   						d = fecha_actual - d;
-		   						d = ((d/1000)/60)/60;
-		   						d = Math.round(d);
+		 //   					if(index == 'pubdate'){
+		 //   						fecha = value;
+		 //   						d = new Date(fecha);
+		 //   						d = fecha_actual - d;
+		 //   						d = ((d/1000)/60)/60;
+		 //   						d = Math.round(d);
 
-		   						if(d > 24){
-		   							d =  d / 24;
-		   							d = Math.round(d);
-		   							if (d==1) {
-		   								d+= ' día';
-		   							} else {
-		   								d+= ' días';
-		   							}
-		   						}else{
-		   							if (d==1) {
-		   								d+= ' hora';
-		   							} else {
-		   								d+= ' horas';
-		   							}
-		   						}
-		   					}
+		 //   						if(d > 24){
+		 //   							d =  d / 24;
+		 //   							d = Math.round(d);
+		 //   							if (d==1) {
+		 //   								d+= ' día';
+		 //   							} else {
+		 //   								d+= ' días';
+		 //   							}
+		 //   						}else{
+		 //   							if (d==1) {
+		 //   								d+= ' hora';
+		 //   							} else {
+		 //   								d+= ' horas';
+		 //   							}
+		 //   						}
+		 //   					}
 		   					
-		   				});
-		   				if (contador < 11) {
-		   					$('.notas').append('<li class="clearfix"><a href="' + link + '"><div class="img_frame"><img src="' + image +'"></div></a><div class="_info"><h5><a href="' + link + '">' + title +'</a></h5><span class="_time">hace ' + d + '</span></div></li>');
-		   				} else {
-		   					$('.notas').append('<li class="hides clearfix"><a href="' + link + '"><div class="img_frame"><img src="' + image +'"></div></a><div class="_info"><h5><a href="' + link + '">' + title +'</a></h5><span class="_time">hace ' + d + '</span></div></li>');
-		   				}
-		   				if (contador == 10) {
-		   					$('.notas').append('<li class="more clearfix"><a><div class="img_frame">+</div></a><div class="_info"><h5><a>Ver más notas relacionadas</a></h5></div></li>');
-		   				}
-		   				contador++;
-		   			});
+		 //   				});
+		 //   				if (contador < 11) {
+		 //   					$('.notas').append('<li class="clearfix"><a href="' + link + '"><div class="img_frame"><img src="' + image +'"></div></a><div class="_info"><h5><a href="' + link + '">' + title +'</a></h5><span class="_time">hace ' + d + '</span></div></li>');
+		 //   				} else {
+		 //   					$('.notas').append('<li class="hides clearfix"><a href="' + link + '"><div class="img_frame"><img src="' + image +'"></div></a><div class="_info"><h5><a href="' + link + '">' + title +'</a></h5><span class="_time">hace ' + d + '</span></div></li>');
+		 //   				}
+		 //   				if (contador == 10) {
+		 //   					$('.notas').append('<li class="more clearfix"><a><div class="img_frame">+</div></a><div class="_info"><h5><a>Ver más notas relacionadas</a></h5></div></li>');
+		 //   				}
+		 //   				contador++;
+		 //   			});
 
 		   			
-		   })
-		   		.fail(function() {
-		      	console.log("fail");
-		   })
-		   		.always(function() {
-		      	console.log("complete");
-   });
+		 //   })
+		 //   		.fail(function() {
+		 //      	console.log("fail");
+		 //   })
+		 //   		.always(function() {
+		 //      	console.log("complete");
+   // });
 });
 
 })(jQuery);
